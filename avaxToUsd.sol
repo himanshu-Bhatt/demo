a7a3cc32fb8c7eba915a4055a67b8b5f9a6c12b9
himanshu-Bhatt <86202656+himanshu-Bhatt@users.noreply.github.com>
2022-04-24 22:21:25 +0530

    Create avaxToUsd.sol

---
+++ avaxToUsd.sol
@@ -0,0 +1,213 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+pragma solidity >=0.8.4;
+
+pragma solidity >=0.8.4;
+
+library StringUtils {
+    /**
+     * @dev Returns the length of a given string
+     *
+     * @param s The string to measure the length of
+     * @return The length of the input string
+     */
+    function strlen(string memory s) internal pure returns (uint) {
+        uint len;
+        uint i = 0;
+        uint bytelength = bytes(s).length;
+        for(len = 0; i < bytelength; len++) {
+            bytes1 b = bytes(s)[i];
+            if(b < 0x80) {
+                i += 1;
+            } else if (b < 0xE0) {
+                i += 2;
+            } else if (b < 0xF0) {
+                i += 3;
+            } else if (b < 0xF8) {
+                i += 4;
+            } else if (b < 0xFC) {
+                i += 5;
+            } else {
+                i += 6;
+            }
+        }
+        return len;
+    }
+}
+/**
+ * @title SafeMath
+ * @dev Unsigned math operations with safety checks that revert on error
+ */
+library SafeMath {
+    /**
+    * @dev Multiplies two unsigned integers, reverts on overflow.
+    */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b);
+
+        return c;
+    }
+
+    /**
+    * @dev Integer division of two unsigned integers truncating the quotient, reverts on division by zero.
+    */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Solidity only automatically asserts when dividing by 0
+        require(b > 0);
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+        return c;
+    }
+
+    /**
+    * @dev Subtracts two unsigned integers, reverts on overflow (i.e. if subtrahend is greater than minuend).
+    */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a);
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    /**
+    * @dev Adds two unsigned integers, reverts on overflow.
+    */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a);
+
+        return c;
+    }
+
+    /**
+    * @dev Divides two unsigned integers and returns the remainder (unsigned integer modulo),
+    * reverts when dividing by zero.
+    */
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b != 0);
+        return a % b;
+    }
+}
+interface PriceOracle {
+    /**
+     * @dev Returns the price to register or renew a name.
+     * @param name The name being registered or renewed.
+     * @param expires When the name presently expires (0 if this is a new registration).
+     * @param duration How long the name is being registered or extended for, in seconds.
+     * @return The price of this renewal or registration, in wei.
+     */
+    function price(string calldata name, uint expires, uint duration) external view returns(uint);
+}
+
+// StablePriceOracle sets a price in USD, based on an oracle.
+contract StablePriceOracle is Ownable, PriceOracle {
+    using SafeMath for *;
+    using StringUtils for *;
+
+    // Rent in base price units by length. Element 0 is for 1-length names, and so on.
+    uint[] public rentPrices;
+
+    // Oracle address
+       AggregatorV3Interface internal usdOracle;
+
+    event OracleChanged(address oracle);
+
+    event RentPriceChanged(uint[] prices);
+
+    bytes4 constant private INTERFACE_META_ID = bytes4(keccak256("supportsInterface(bytes4)"));
+    bytes4 constant private ORACLE_ID = bytes4(keccak256("price(string,uint256,uint256)") ^ keccak256("premium(string,uint256,uint256)"));
+
+        constructor(AggregatorV3Interface _usdOracle, uint[] memory _rentPrices) {
+        usdOracle = _usdOracle;
+        setPrices(_rentPrices);
+    }
+
+    function price(string calldata name, uint expires, uint duration) external view override returns(uint) {
+        uint len = name.strlen();
+        if(len > rentPrices.length) {
+            len = rentPrices.length;
+        }
+        require(len > 0);
+        
+        uint basePrice = rentPrices[len - 1].mul(duration);
+        basePrice = basePrice.add(_premium(name, expires, duration));
+
+        return attoUSDToAvax(basePrice);
+    }
+
+    /**
+     * @dev Sets rent prices.
+     * @param _rentPrices The price array. Each element corresponds to a specific
+     *                    name length; names longer than the length of the array
+     *                    default to the price of the last element. Values are
+     *                    in base price units, equal to one attodollar (1e-18
+     *                    dollar) each.
+     */
+    function setPrices(uint[] memory _rentPrices) public onlyOwner {
+        rentPrices = _rentPrices;
+        emit RentPriceChanged(_rentPrices);
+    }
+
+    /**
+     * @dev Sets the price oracle address
+     * @param _usdOracle The address of the price oracle to use.
+     */
+    function setOracle(AggregatorV3Interface _usdOracle) public onlyOwner {
+        usdOracle = _usdOracle;
+        emit OracleChanged(address(_usdOracle));
+    }
+
+    /**
+     * @dev Returns the pricing premium in wei.
+     */
+    function premium(string calldata name, uint expires, uint duration) external view returns(uint) {
+        return attoUSDToAvax(_premium(name, expires, duration));
+    }
+
+    /**
+     * @dev Returns the pricing premium in internal base units.
+     */
+    function _premium(string memory name, uint expires, uint duration) virtual internal view returns(uint) {
+        return 0;
+    }
+
+    function attoUSDToAvax(uint amount) public view returns(uint) {
+        (
+            /*uint80 roundID*/,
+            int avaxPrice,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = usdOracle.latestRoundData();
+        return amount.mul(1e8).div(uint256(avaxPrice));
+    }
+
+    function avaxToAttoUSD(uint amount) public view returns(uint) {
+        (
+            /*uint80 roundID*/,
+            int avaxPrice,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = usdOracle.latestRoundData();
+        return amount.mul(uint256(avaxPrice)).div(1e8);
+    }
+
+    function supportsInterface(bytes4 interfaceID) public view virtual returns (bool) {
+        return interfaceID == INTERFACE_META_ID || interfaceID == ORACLE_ID;
+    }
+}
